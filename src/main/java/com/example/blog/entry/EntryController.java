ed9aa3854177eb92678bf9fa9101092520c6aec0
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2018-02-01 12:00:00 +0900

    first commit

--- src/main/java/com/example/blog/entry/EntryController.java
+++ src/main/java/com/example/blog/entry/EntryController.java
@@ -5,25 +5,27 @@ import java.util.List;
 import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryId;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class EntryController {
-	private final EntryRepository entryRepository;
+    private final EntryRepository entryRepository;
 
-	public EntryController(EntryRepository entryRepository) {
-		this.entryRepository = entryRepository;
-	}
+    public EntryController(EntryRepository entryRepository) {
+        this.entryRepository = entryRepository;
+    }
 
-	@GetMapping("v1/entries")
-	public List<Entry> getEntries() {
-		return null; // TODO: 演習1
-	}
+    @GetMapping("v1/entries")
+    public List<Entry> getEntries() {
+        return entryRepository.findAll();
+    }
 
-	@GetMapping("v1/entries/{entryId}")
-	public Entry getEntry(@PathVariable EntryId entryId) {
-		return null; // TODO: 演習1
-	}
+    @GetMapping("v1/entries/{entryId}")
+    public Entry getEntry(@PathVariable EntryId entryId) {
+        return entryRepository.findById(entryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "EntryId " + entryId + " is not found."));
+    }
 }
--- src/main/java/com/example/blog/webhook/WebhookController.java
+++ src/main/java/com/example/blog/webhook/WebhookController.java
@@ -10,6 +10,10 @@ import java.util.stream.StreamSupport;
 import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryId;
 import com.example.blog.BlogProperties;
+import com.example.blog.entry.EntryRepository;
+import com.example.blog.entry.event.EntryCreateEvent;
+import com.example.blog.entry.event.EntryDeleteEvent;
+import com.example.blog.entry.event.EntryUpdateEvent;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import reactor.core.publisher.Flux;
@@ -22,50 +26,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebhookController {
-	private final EntryFetcher entryFetcher;
-	private final ApplicationEventPublisher publisher;
-	private final WebhookVerifier webhookVerifier;
-	private final ObjectMapper objectMapper;
+    private final EntryFetcher entryFetcher;
+    private final ApplicationEventPublisher publisher;
+    private final WebhookVerifier webhookVerifier;
+    private final ObjectMapper objectMapper;
 
-	public WebhookController(BlogProperties props, EntryFetcher entryFetcher,
-			ApplicationEventPublisher publisher, ObjectMapper objectMapper)
-			throws NoSuchAlgorithmException, InvalidKeyException {
-		this.entryFetcher = entryFetcher;
-		this.publisher = publisher;
-		this.objectMapper = objectMapper;
-		this.webhookVerifier = new WebhookVerifier(props.getGithub().getWebhookSecret());
-	}
+    public WebhookController(BlogProperties props, EntryFetcher entryFetcher,
+                             ApplicationEventPublisher publisher, ObjectMapper objectMapper, EntryRepository entryRepository)
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        this.entryFetcher = entryFetcher;
+        this.publisher = publisher;
+        this.objectMapper = objectMapper;
+        this.webhookVerifier = new WebhookVerifier(props.getGithub().getWebhookSecret());
+    }
 
-	@PostMapping("webhook")
-	public Flux<?> webhook(@RequestBody String payload,
-			@RequestHeader("X-Hub-Signature") String signature) throws Exception {
-		this.webhookVerifier.verify(payload, signature);
-		JsonNode node = this.objectMapper.readValue(payload, JsonNode.class);
-		String[] repository = node.get("repository").get("full_name").asText().split("/");
-		String owner = repository[0];
-		String repo = repository[1];
-		Stream<JsonNode> commits = StreamSupport.stream(node.get("commits").spliterator(),
-				false);
-		// TODO: 演習2
-		return Flux.fromStream(commits).flatMap(commit -> {
-			Flux<EntryId> added = this.paths(commit.get("added"))
-					.flatMap(path -> this.entryFetcher.fetch(owner, repo, path)) //
-					.map(Entry::entryId);
-			Flux<EntryId> modified = this.paths(commit.get("modified")) //
-					.flatMap(path -> this.entryFetcher.fetch(owner, repo, path)) //
-					.map(Entry::entryId);
-			Flux<EntryId> removed = this.paths(commit.get("removed")) //
-					.map(path -> EntryId.fromFilePath(Paths.get(path)));
-			return added.map(id -> Collections.singletonMap("added", id.getValue())) //
-					.mergeWith(modified.map(
-							id -> Collections.singletonMap("modified", id.getValue()))) //
-					.mergeWith(removed.map(
-							id -> Collections.singletonMap("removed", id.getValue())));
-		});
-	}
+    @PostMapping("webhook")
+    public Flux<?> webhook(@RequestBody String payload,
+                           @RequestHeader("X-Hub-Signature") String signature) throws Exception {
+        this.webhookVerifier.verify(payload, signature);
+        JsonNode node = this.objectMapper.readValue(payload, JsonNode.class);
+        String[] repository = node.get("repository").get("full_name").asText().split("/");
+        String owner = repository[0];
+        String repo = repository[1];
+        Stream<JsonNode> commits = StreamSupport.stream(node.get("commits").spliterator(),false);
+        return Flux.fromStream(commits).flatMap(commit -> {
+            Flux<EntryId> added = this.paths(commit.get("added"))
+                    .flatMap(path -> this.entryFetcher.fetch(owner, repo, path)) //
+                    .doOnNext(entry -> {
+                        publisher.publishEvent(new EntryCreateEvent(entry));
+                    })
+                    .map(Entry::entryId);
+            Flux<EntryId> modified = this.paths(commit.get("modified")) //
+                    .flatMap(path -> this.entryFetcher.fetch(owner, repo, path)) //
+                    .doOnNext(entry -> {
+                        publisher.publishEvent(new EntryUpdateEvent(entry));
+                    })
+                    .map(Entry::entryId);
+            Flux<EntryId> removed = this.paths(commit.get("removed")) //
+                    .map(path -> EntryId.fromFilePath(Paths.get(path)))
+                    .doOnNext(entry -> {
+                        publisher.publishEvent(new EntryDeleteEvent(entry));
+                    });
+            return added.map(id -> Collections.singletonMap("added", id.getValue())) //
+                    .mergeWith(modified.map(
+                            id -> Collections.singletonMap("modified", id.getValue()))) //
+                    .mergeWith(removed.map(
+                            id -> Collections.singletonMap("removed", id.getValue())));
+        });
+    }
 
-	Flux<String> paths(JsonNode paths) {
-		return Flux.fromStream(
-				StreamSupport.stream(paths.spliterator(), false).map(JsonNode::asText));
-	}
+    Flux<String> paths(JsonNode paths) {
+        return Flux.fromStream(
+                StreamSupport.stream(paths.spliterator(), false).map(JsonNode::asText));
+    }
 }
